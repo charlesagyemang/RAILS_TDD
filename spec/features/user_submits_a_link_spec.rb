e93ef9f145a65a157f388ab4554a50e61fb66b03
charlesagyemang <micnkru@gmail.com>
2021-07-07 03:02:18 +0000

    run scenario on validations

--- spec/features/user_submits_a_link_spec.rb
+++ spec/features/user_submits_a_link_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "User Submits A Form" do
+
     scenario "they see the page for the submit link" do
 
         link_title = "This Testing Rails book is awesome!" 
@@ -15,4 +16,21 @@ RSpec.feature "User Submits A Form" do
 
         expect(page).to have_link link_title, href: link_url
     end
+end
+
+
+context "the form is invalid" do
+    scenario "they see a useful error message" do
+        link_title = "Thid testing is cool"
+
+        visit root_path
+        click_on "Submit a new link"
+        fill_in "link_title",	with: link_title
+        
+        click_on "Submit!"
+
+        puts "======= PAGE =======, #{page.has_content?}"
+
+        expect(page).to have_content "url can't be blank"
+    end    
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Link, "validations" do
+  it { expect(subject).to validate_presence_of(:title) }
+  it { expect(subject).to validate_presence_of(:url) }
 end
